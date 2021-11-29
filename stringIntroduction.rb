ed21e6286f527e2418b32c9d6b7d040c6996d9a1
Đỗ Thanh Tùng <56503231+nomian2001@users.noreply.github.com>
2021-11-29 08:56:40 +0700

    36th exercise

---
+++ stringIntroduction.rb
@@ -0,0 +1,16 @@
+def single_quote
+    # single quote string here
+      'Hello World and others!'
+  end
+  
+  def double_quote
+    # Double quote string here
+      "Hello World and others!"
+  end
+  
+  def here_doc
+    # Here doc string here
+      <<-DTT
+          Hello World and others!
+      DTT
+  end
